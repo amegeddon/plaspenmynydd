5c731a54f9cce1f2054c08e301ded7cb331fd3b1
amegeddon <ameescook@gmail.com>
2024-01-23 20:18:28 +0000

    Adds sendmail function

--- assets/sendEmail.js
+++ assets/sendEmail.js
@@ -1,2 +1,15 @@
-console.log("hello");
-console.log("is it me your looking for?");
+function sendMail(contactform) {
+    emailjs.send("service_naeyqkf", "template_nbukpv4", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.name.emailaddress.value,
+        "message": conactForm.message.value,
+    }}
+    .then(
+        function(response) {
+            console.log("success", response)
+        }
+        function(error) {
+            console.log("Error"),reponse {
+
+        }};
+ )
