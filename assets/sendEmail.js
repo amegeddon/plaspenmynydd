91f0cd65ca6fe3ca85d6f3a1f4d9d6abab9ed1a8
amegeddon <ameescook@gmail.com>
2024-01-23 20:30:28 +0000

    Updates js function for sending contact form

--- assets/sendEmail.js
+++ assets/sendEmail.js
@@ -1,15 +1,13 @@
-function sendMail(contactform) {
-    emailjs.send("service_naeyqkf", "template_nbukpv4", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.name.emailaddress.value,
-        "message": conactForm.message.value,
-    }}
-    .then(
-        function(response) {
-            console.log("success", response)
-        }
-        function(error) {
-            console.log("Error"),reponse {
 
-        }};
- )
+
+ var templateParams = {
+    name: 'Amy',
+    notes: 'Check this out!'
+};
+ 
+emailjs.send('service_naeyqkf', 'template_nbukpv4', templateParams)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
