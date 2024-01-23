a4b21c15065462a63b733dcd3678419dccfdc569
amegeddon <ameescook@gmail.com>
2024-01-23 21:46:09 +0000

    Adjust js for sending message

--- assets/sendEmail.js
+++ assets/sendEmail.js
@@ -1,13 +1,30 @@
 
 
- var templateParams = {
+ /*var templateParams = {
     name: 'Amy',
     notes: 'Check this out!'
-};
+};*/
+
+function sendMail(contactForm) {
+    emailjs.send("service_naeyqkf", "template_nbukpv4", {
+        "from_name": contactForm.nameinput.value,
+        "from_email": contactForm.emailinputs.value,
+        "project_request": contactForm.messagearea.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
  
-handleFormSubmission(form)('service_naeyqkf', 'template_nbukpv4', templateParams)
+/*handleFormSubmission(form)('service_naeyqkf', 'template_nbukpv4', templateParams)
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
     }, function(error) {
        console.log('FAILED...', error);
-    });
+    });*/
